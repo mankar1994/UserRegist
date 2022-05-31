7a6609f996ce6ed2bcad788b553f5e2f7e6d8722
ajit <palajitsingh91@gmail.com>
2022-06-01 03:04:19 +0530

    Add project files.

---
+++ UserRegist/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegist.Models;
+
+namespace UserRegist.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Edit(int id=0)
+        {
+            User user = new User();
+
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            using(DBModels db=new DBModels())
+            {
+                if (db.Users.Any(x => x.UserName == user.UserName))
+                {
+                    ViewBag.DuplicateMessage = "User Name Already Exist ";
+                    return View("Edit", user);
+                }
+                else
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration SuccessFull";
+            return View("Edit",new User());
+        }
+    }
+}
